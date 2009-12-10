2d042e15ae854eb460705687ba8889cec43a5e5e
Stefan Tomanek <stefan.tomanek@wertarbyte.de>
2009-12-10 21:29:32 +0100

    add timezone offset

--- calfilter.pl
+++ calfilter.pl
@@ -6,12 +6,35 @@ use utf8;
 use CGI;
 use Data::ICal;
 use LWP::Simple;
+use DateTime;
 
 my $q = new CGI();
 
 my $url = $q->param("url");
 my $regex = ($q->param("regex") || '');
 my $name = ($q->param("name") || '');
+my $tzoffset = 0;
+if (defined $q->param("tzoffset") && $q->param("tzoffset") =~ /^[+-]?[0-9]+$/) {
+    $tzoffset += $q->param("tzoffset");
+}
+
+sub add_offset {
+    my ($timestring, $offset) = @_;
+    if ($timestring =~ /^([0-9]{4})([0-9]{2})([0-9]{2})T([0-9]{2})([0-9]{2})([0-9]{2})$/) {
+        my $dt = DateTime->new( year => $1, month => $2, day => $3, hour => $4, minute => $5, second => $6 );
+        $dt = $dt->add( minutes => $offset );
+
+        return $dt->strftime('%Y%m%dT%H%M%S');
+    }
+    return $timestring;
+}
+
+sub change_tz_entry {
+    my ($e, $key, $offset) = @_;
+    my $p = $e->property($key)->[0];
+    my $new = add_offset($p->decoded_value(), $tzoffset);
+    $p->value($new);
+}
 
 my $data = get($url);
 
@@ -27,6 +50,9 @@ if ($ocal) {
     
     for my $e (@{$ocal->entries}) {
         if ($e->property('SUMMARY')->[0]->as_string =~ $regex) {
+            change_tz_entry( $e, "DTSTART", $tzoffset);
+            change_tz_entry( $e, "DTEND", $tzoffset);
+            
             $ncal->add_entry($e);
         }
     }
